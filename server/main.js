7a5331bda5206b581411a8e893d997a5951e7f5c
Nestor <alu0100812503@ull.edu.es>
2017-02-25 20:41:13 +0000

    Tuto tutoriali

--- server/main.js
+++ server/main.js
@@ -3,25 +3,29 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-
-server.listen(8080, function() {
-    console.log('Servidor corriendo en http://localhost:8080');
-});
-
 var messages = [{
-    author: "Autor 1",
-    text: "Mensaje 1"
-},{
-    author: "Autor 2",
-    text: "Mensaje 2"
-},{
-    author: "Autor 3",
-    text: "Mensaje 3"
-}]
-
-io.on('connection', function(socket) {
-    console.log('Un cliente se ha conectado');
-    socket.emit('messages', messages);
-});
+  id: 1,
+  text: "Hola soy un mensaje",
+  author: "Autor"
+}];
 
 app.use(express.static('public'));
+
+app.get('/hello', function(req, res) {
+  res.status(200).send("Hello World!");
+});
+
+io.on('connection', function(socket) {
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
+
+  socket.on('new-message', function(data) {
+    messages.push(data);
+
+    io.sockets.emit('messages', messages);
+  });
+});
+
+server.listen(8080, function() {
+  console.log("Servidor corriendo en http://localhost:8080");
+});
